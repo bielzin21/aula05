cca2984fb33dd0247449eb2f02eb7b25316bba62
bielzin21 <rm83826@estudante.fieb.edu.br>
2024-05-15 18:21:46 -0300

    DOM

--- js/main.js
+++ js/main.js
@@ -1,36 +1,8 @@
-// let hora = "14"
+let div = document.querySelector(".index")
+let p = document.createElement("p")
+let li = document.createElement("li")
 
-// if(hora <= 4 && hora >= 0) {
-//     console.log('Boa madrugada!');
-// } else if (hora >= 5 && hora < 12) {
-//     console.log('Bom dia!');
-// } else if (hora >= 12 && hora <= 18) {
-//     console.log('Boa tarde');
-// } else {
-//     console.log('Boa noite!');
-// }
-
-const playerOne = "rock"
-const computer = "scissors"
-
-if (playerOne === computer) {
-    console.log("empate")
-} else if (playerOne === "rock") {
-    if(computer === "paper"){
-        console.log('Computador ganhou!')
-    } else {
-        console.log('playerOne ganhou!')
-    }
-} else if (playerOne === "paper") {
-    if(computer === "scissors") {
-        console.log('Computador ganhou!')
-    }else {
-        console.log('playerOne ganhou!')
-    }
-} else {
-    if (computer === "rock") {
-        console.log('Computador ganhou!')
-    }else {
-        console.log('playerOne ganhou!')
-    }
-}
+p.innerHTML = "Olá, mundo!"
+li.innerHTML = "oi, mundo!"
+div.appendChild(p)
+div.appendChild(li)
